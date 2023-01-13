f5a3a2248cec06570db71821ad251d947deeaa0c
Ramakant Gangwar <14928642+rxcod9@users.noreply.github.com>
2023-01-13 10:29:11 +0530

    DEPS Override voyager assets and compile Start using joy-voyager-core as Parent classes Change master layout to use compiled asset Add relations table in read view Override relationship blade to allow morphTo

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,4 @@
-const mix = require('laravel-mix');
+let mix = require('laravel-mix');
 
 /*
  |--------------------------------------------------------------------------
@@ -6,15 +6,16 @@ const mix = require('laravel-mix');
  |--------------------------------------------------------------------------
  |
  | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel applications. By default, we are compiling the CSS
+ | for your Laravel application. By default, we are compiling the Sass
  | file for the application as well as bundling up all the JS files.
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ])
-    .postCss('node_modules/@fortawesome/fontawesome-free/css/all.css', 'public/css/fontawesome.css', [
-        //
-    ]);
+mix.options({ processCssUrls: false }).sass('resources/assets/sass/app.scss', 'public/assets/css')
+.js('resources/assets/js/app.js', 'public/assets/js').vue({ version: 2 })
+.copy('node_modules/tinymce/skins', 'public/assets/js/skins')
+.copy('resources/assets/js/skins', 'public/assets/js/skins')
+.copy('node_modules/tinymce/themes/silver', 'public/assets/js/themes/silver')
+.copy('node_modules/tinymce/models/dom', 'public/assets/js/models/dom')
+.copy('node_modules/tinymce/icons/default', 'public/assets/js/icons/default')
+.copy('node_modules/ace-builds/src-noconflict', 'public/assets/js/ace/libs');
